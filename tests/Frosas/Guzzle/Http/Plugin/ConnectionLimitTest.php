1fba71bd484fcd5a542dd095de3856d1a1ea9a9d
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-03-31 16:57:10 +0200

    Unused "use"

--- tests/Frosas/Guzzle/Http/Plugin/ConnectionLimitTest.php
+++ tests/Frosas/Guzzle/Http/Plugin/ConnectionLimitTest.php
@@ -3,7 +3,6 @@
 namespace Frosas\Guzzle\Http\Plugin;
 
 use Guzzle\Http\Message\Response;
-use Guzzle\Http\Plugin\MockPlugin;
 
 class ConnectionLimitPluginTest extends \PHPUnit_Framework_TestCase
 {
